303c3f444a4b490414acf07d772223f4408f7470
shafahadshah <pirshahfahadshah@gmail.com>
2026-04-06 12:11:54 +0500

    add basic arrays

---
+++ 04-arrays-tuples/basic-arrays.ts
@@ -0,0 +1,10 @@
+// Basic array types
+
+let numbers: number[] = [1, 2, 3, 4];
+let fruits: string[] = ["apple", "banana", "mango"];
+
+numbers.push(5);
+fruits.push("orange");
+
+console.log(numbers);
+console.log(fruits);
