50596c299b526a3af93705d051470053094e7955
shafahadshah <pirshahfahadshah@gmail.com>
2026-04-09 15:14:58 +0500

    add type narrowing logic

---
+++ 07-union-types/type-narrowing.ts
@@ -0,0 +1,10 @@
+function formatValue(value: string | number): string {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else {
+    return value.toFixed(2);
+  }
+}
+
+console.log(formatValue("hello"));
+console.log(formatValue(12.345));
