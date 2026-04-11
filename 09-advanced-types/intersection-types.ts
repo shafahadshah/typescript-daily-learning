cd669f3f18bf6fea81f33771da1356b2f5c636ad
shafahadshah <pirshahfahadshah@gmail.com>
2026-04-11 15:48:34 +0500

    init intersection types

---
+++ 09-advanced-types/intersection-types.ts
@@ -0,0 +1,18 @@
+// Intersection types
+
+type Person = {
+  name: string;
+};
+
+type Employee = {
+  id: number;
+};
+
+type Staff = Person & Employee;
+
+const staff: Staff = {
+  name: "Ali",
+  id: 101
+};
+
+console.log(staff);
