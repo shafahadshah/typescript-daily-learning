4d343326fb23780c5a567a14f42bbce51368ac89
shafahadshah <pirshahfahadshah@gmail.com>
2026-04-26 15:05:42 +0500

    basic constructor types

---
+++ 20-constructors-types/basic-constructor.ts
@@ -0,0 +1,14 @@
+// Basic constructor with types
+
+class User {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const u = new User("Ali", 25);
+console.log(u);
