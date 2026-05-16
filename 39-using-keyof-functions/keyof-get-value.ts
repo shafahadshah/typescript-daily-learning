ff8f7fb01fba57fc153e25abf5897845857f87fb
shafahadshah <pirshahfahadshah@gmail.com>
2026-05-16 09:49:47 +0500

    safe object property access

---
+++ 39-using-keyof-functions/keyof-get-value.ts
@@ -0,0 +1,22 @@
+function getValue<
+  T,
+  K extends keyof T
+>(
+  obj: T,
+  key: K
+) {
+  return obj[key];
+}
+
+const user = {
+  name: "Ali",
+  age: 25,
+};
+
+console.log(
+  getValue(user, "name")
+);
+
+console.log(
+  getValue(user, "age")
+);
