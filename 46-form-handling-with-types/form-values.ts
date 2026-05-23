7ddc224c842935ebcf206d768946250f0a26ea82
shafahadshah <pirshahfahadshah@gmail.com>
2026-05-23 15:12:38 +0500

    extracting typed values

---
+++ 46-form-handling-with-types/form-values.ts
@@ -0,0 +1,17 @@
+const emailInput =
+  document.querySelector(
+    "#email"
+  ) as HTMLInputElement;
+
+const passwordInput =
+  document.querySelector(
+    "#password"
+  ) as HTMLInputElement;
+
+console.log(
+  emailInput.value
+);
+
+console.log(
+  passwordInput.value
+);
