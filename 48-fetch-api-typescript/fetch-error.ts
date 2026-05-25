31baac47e259148136a02c813d1f4ff0a235b8a7
shafahadshah <pirshahfahadshah@gmail.com>
2026-05-25 15:09:50 +0500

    fetch error handling

---
+++ 48-fetch-api-typescript/fetch-error.ts
@@ -0,0 +1,24 @@
+async function getData() {
+  try {
+    const response =
+      await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+    if (!response.ok) {
+      throw new Error(
+        "Request failed"
+      );
+    }
+
+    const data =
+      await response.json();
+
+    console.log(data);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData();
