19601ccf05662c8bd65a8fe74855609d96594760
shafahadshah <pirshahfahadshah@gmail.com>
2026-05-30 12:13:52 +0500

    typed error handler

---
+++ 53-express-server-with-types/src/errorHandler.ts
@@ -0,0 +1,16 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  res.status(500).json({
+    message: err.message,
+  });
+}
