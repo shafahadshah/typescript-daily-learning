dd4f60e3aaa3c346f9f6a5cc38b26ec1936db712
shafahadshah <pirshahfahadshah@gmail.com>
2026-05-31 21:11:59 +0500

    basic Request & Response typing

---
+++ 54-typed-request-response/request-response.ts
@@ -0,0 +1,18 @@
+import express, {
+  Request,
+  Response,
+} from "express";
+
+const app = express();
+
+app.get(
+  "/",
+  (
+    req: Request,
+    res: Response
+  ) => {
+    res.send("Hello");
+  }
+);
+
+app.listen(3000);
