e9a0faa25c42864a2f788846f4ccc35634b38368
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-01 12:11:02 +0500

    basic middleware

---
+++ 55-middleware-typing/basic-middleware.ts
@@ -0,0 +1,17 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+export function logger(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(
+    req.method
+  );
+
+  next();       
+}
