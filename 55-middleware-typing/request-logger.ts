ebb4bcf1918914a4635d891b6a3d931fe2566bc2
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-01 12:11:56 +0500

    request logger middleware

---
+++ 55-middleware-typing/request-logger.ts
@@ -0,0 +1,17 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+export function requestLogger(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(
+    `${req.method} ${req.url}`
+  );
+
+  next();
+}
