4ffc69f5878d7c9c5525cfd5bf5792377208b56f
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-03 09:11:12 +0500

    basic authentication middleware

---
+++ 57-authentication-middleware-typing/auth-middleware.ts
@@ -0,0 +1,22 @@
+import {
+  Request,
+  Response,
+  NextFunction,
+} from "express";
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token =
+    req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  next();
+}
