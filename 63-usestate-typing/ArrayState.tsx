0160aabbc73c4dafbb947f5dd49f758b9c947050
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-09 18:15:30 +0500

    array state

---
+++ 63-usestate-typing/ArrayState.tsx
@@ -0,0 +1,28 @@
+import { useState }
+from "react";
+
+function ArrayState() {
+  const [
+    items,
+    setItems,
+  ] = useState<string[]>(
+    []
+  );
+
+  function addItem() {
+    setItems([
+      ...items,
+      "New Item",
+    ]);
+  }
+
+  return (
+    <button
+      onClick={addItem}
+    >
+      {items.length}
+    </button>
+  );
+}
+
+export default ArrayState;
