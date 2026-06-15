e10832a2ecf3e7810f4e06e383b21baa5360baf1
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-15 15:29:29 +0500

    basic WebSocket server

---
+++ 69-websocket-server-typescript/server.ts
@@ -0,0 +1,21 @@
+import {
+  WebSocketServer,
+} from "ws";
+
+const wss =
+  new WebSocketServer({
+    port: 8080,
+  });
+
+wss.on(
+  "connection",
+  () => {
+    console.log(
+      "Client connected"
+    );
+  }
+);
+
+console.log(
+  "WebSocket running"
+);
