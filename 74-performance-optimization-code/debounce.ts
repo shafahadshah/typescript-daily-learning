92a2bab13c5f09e88c45dd33b7a12013ee9d807b
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-20 15:22:09 +0500

    debounce utility

---
+++ 74-performance-optimization-code/debounce.ts
@@ -0,0 +1,29 @@
+export function debounce(
+  callback: Function,
+  delay: number
+) {
+
+  let timer:
+    ReturnType<
+      typeof setTimeout
+    >;
+
+  return (
+    ...args: unknown[]
+  ) => {
+
+    clearTimeout(
+      timer
+    );
+
+    timer =
+      setTimeout(
+        () => {
+          callback(
+            ...args
+          );
+        },
+        delay
+      );
+  };
+}
