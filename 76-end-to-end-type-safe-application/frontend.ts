a9d14fd45664253c86faa68313be48a7ec6fe4f5
shafahadshah <pirshahfahadshah@gmail.com>
2026-06-22 20:18:52 +0500

    frontend request typing

---
+++ 76-end-to-end-type-safe-application/frontend.ts
@@ -0,0 +1,19 @@
+import {
+  User,
+  ApiResponse,
+} from "./shared-types";
+
+const response:
+  ApiResponse<User> = {
+    success: true,
+
+    data: {
+      id: 1,
+      name: "Ali",
+      email: "ali@test.com",
+    },
+  };
+
+console.log(
+    response.data.name
+);
