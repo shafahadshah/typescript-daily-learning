53b10cb0cd8015720c876a28e51b7b1ee77e9d56
shafahadshah <pirshahfahadshah@gmail.com>
2026-07-02 13:26:17 +0500

    product DTO

---
+++ 86-creating-reusable-dtos/product-dto.ts
@@ -0,0 +1,13 @@
+type ProductDto = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+const product: ProductDto = {
+  id: 101,
+  title: "Laptop",
+  price: 1200,
+};
+
+console.log(product);
