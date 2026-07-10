7a739ae95ddd7d2d3df28c97b8068f8932069256
shafahadshah <pirshahfahadshah@gmail.com>
2026-07-10 12:40:03 +0500

    application entry

---
+++ 94-feature-based-folder-structure/app.ts
@@ -0,0 +1,20 @@
+import { User }
+from "./user";
+
+import { Product }
+from "./product";
+
+const user: User = {
+  id: 1,
+  name: "Ali",
+};
+
+const product: Product = {
+  id: 101,
+  title: "Laptop",
+};
+
+console.log(
+  user,
+  product
+);
