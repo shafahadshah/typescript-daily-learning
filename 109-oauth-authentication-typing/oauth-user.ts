5543eba800cd4f5101177a781b9a41b2ce9f8da4
shafahadshah <pirshahfahadshah@gmail.com>
2026-07-29 19:06:00 +0500

    OAuth user

---
+++ 109-oauth-authentication-typing/oauth-user.ts
@@ -0,0 +1,14 @@
+type OAuthUser = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const user:
+  OAuthUser = {
+    id: "1",
+    name: "Ali",
+    email: "ali@test.com",
+};
+
+console.log(user);
