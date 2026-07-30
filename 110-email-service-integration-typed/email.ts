d3c8e99818a4387a7697ffdb607e925fec351b58
shafahadshah <pirshahfahadshah@gmail.com>
2026-07-30 19:09:26 +0500

    email model

---
+++ 110-email-service-integration-typed/email.ts
@@ -0,0 +1,13 @@
+type Email = {
+  to: string;
+  subject: string;
+  body: string;
+};
+
+const email: Email = {
+  to: "user@test.com",
+  subject: "Welcome",
+  body: "Hello Ali!",
+};
+
+console.log(email);
