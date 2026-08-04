c644bfd7b165888ede1b090eff49cb1a492185ae
shafahadshah <pirshahfahadshah@gmail.com>
2026-08-04 19:02:42 +0500

    application entry

---
+++ 113-notification-system-typing/app.ts
@@ -0,0 +1,15 @@
+type Notification = {
+  title: string;
+  message: string;
+};
+
+const notification:
+  Notification = {
+    title: "Reminder",
+    message:
+      "Meeting at 10 AM",
+};
+
+console.log(
+  notification
+);
