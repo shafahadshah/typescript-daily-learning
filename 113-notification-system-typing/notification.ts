93bc1ea49ff618e55c172c39b8c4b268736ebfbc
shafahadshah <pirshahfahadshah@gmail.com>
2026-08-04 19:00:13 +0500

    notification model

---
+++ 113-notification-system-typing/notification.ts
@@ -0,0 +1,16 @@
+type Notification = {
+  id: number;
+  title: string;
+  message: string;
+};
+
+const notification:
+  Notification = {
+    id: 1,
+    title: "Welcome",
+    message: "Hello Ali!",
+};
+
+console.log(
+  notification
+);
